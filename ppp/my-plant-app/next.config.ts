a832f81e38b51cb594da867a882cb9dbeb7e2649
Sai Harish <saiharish0088@gmail.com>
2025-05-24 12:58:13 +0000

    i got this error why when i run npm run dev

---
+++ ppp/my-plant-app/next.config.ts
@@ -0,0 +1,44 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'perenual.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ["https://9003-firebase-studio-1747161752318.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev"],
+  },
+};
+
+export default nextConfig;
