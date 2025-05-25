944f19b663ad93352cf6f1f4c558c738403cebb4
Sai Harish <saiharish0088@gmail.com>
2025-05-25 08:13:29 +0000

    Uncaught Error: React is not defined     at CultivationGuideForm (cultivation-guide-form.tsx:42:3)     at SuggestionsPage (page.tsx:189:15)     at ClientPageRoot (client-page.tsx:60:12) account:1  [Deprecation] -ms-high-contrast is in the process of being deprecated. Please see https://blogs.windows.com/msedgedev/2024/04/29/deprecating-ms-high-contrast/ for tips on updating to the new Forced Colors Mode standard. account:1  [Deprecation] -ms-high-contrast is in the process of being deprecated. Please see https://blogs.windows.com/msedgedev/2024/04/29/deprecating-ms-high-contrast/ for tips on updating to the new Forced Colors Mode standard.
             Microsoft Edge is moving towards a new experience that allows users to choose to browse without third-party cookies. im getting this error

--- ppp/my-plant-app/src/components/suggestions/cultivation-guide-form.tsx
+++ ppp/my-plant-app/src/components/suggestions/cultivation-guide-form.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import * as React from 'react'; // Added React import
+import * as React from 'react'; // Ensure React is imported
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
